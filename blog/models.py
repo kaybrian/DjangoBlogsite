2038b0e789385ba4e92a433953de6e6c7618d507
kaybrian <kayonbrian@gmail.com>
2020-06-04 21:05:09 +0300

    model page for the editors choice is done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EditorsPick(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    tag = models.CharField(max_length=50)
+    date_Created = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='Editors_pick')
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def imageUrl(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
